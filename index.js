944464855387ca4387d9a72c02291ee41d43147e
jesicerpdev <180146@gmail.com>
2022-06-10 20:16:43 -0500

    reduce method description and example

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // reduce executes a reduce function on each element of an array, returning a single value as a result.
 //use case sum of final average
-const array1 = [1, 2, 3, 4];
+const array1 = [1, 2, 3, 4, 5];
 
 // 0 + 1 + 2 + 3 + 4
 const initialValue = 0;

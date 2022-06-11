8bb348d4e93b97b6e79b52cc8721f5377fd3556d
jesicerpdev <180146@gmail.com>
2022-06-10 20:11:50 -0500

    find method description and example

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // find returns the value of the first element of the array that satisfies the given test function.
 //use case number of marbles collected
-const array1 = [5, 12, 8, 130, 44];
+const array1 = [5, 12, 8, 130, 44,54];
 
 const found = array1.find(element => element > 10);
 

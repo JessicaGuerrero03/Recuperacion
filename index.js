eb278a750bf90cbfcb5d162f477a4253e6f08fbf
jesicerpdev <180146@gmail.com>
2022-06-10 20:26:07 -0500

    replace method description and example

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //use case select some pet
 const p = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
 
-console.log(p.replace('dog', 'monkey'));
+console.log(p.replace('dog', 'monkey', 'cat'));
 // expected output: "The quick brown fox jumps over the lazy monkey. If the dog reacted, was it really lazy?"
 
 const regex = /Dog/i;
